ba0ecb8254013aea0c142dcd2b9f1241519507f1
Zulex <hizirwan@hotmail.com>
2024-07-14 17:39:10 +0000

    scroll through images

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_image_comparison import image_comparison
+import os
 
 st.set_page_config(page_title="Image-Comparison Example", layout="centered")
 
@@ -8,7 +9,6 @@ st.write("This page presents a visualization of our project aimed at estimating 
 st.write("I use three different models in three steps. First I detect the object and its bounding box. Secondly I increase the resolution and lastly I detect the individual points of interest.")
 
 
-imageName = "100.png"
 box = "data/box/"
 crop = "data/crop/"
 cropPose = "data/cropPose/"
@@ -16,6 +16,58 @@ initial = "data/initial/"
 super = "data/super/"
 superPose = "data/superPose/"
 
+col1, col2, col3 = st.columns(3)
+
+
+
+# Function to get the list of image names
+def get_image_names(directory):
+    return sorted([name for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))])
+
+# Get the image names
+image_names = get_image_names(initial)
+
+if 'current_index' not in st.session_state:
+    st.session_state.current_index = 0
+
+
+if 'imageName' not in st.session_state:
+    st.session_state.imageName = "100.png"
+
+# Function to update the index and handle bounds
+def update_index(delta):
+    st.session_state.current_index = (st.session_state.current_index + delta) % len(image_names)
+
+def display_filename():
+    filename = image_names[st.session_state.current_index]
+    with col2:
+        st.write(filename)
+
+def next_quote():
+    if st.session_state.current_index + 1 >= len(image_names):
+        st.session_state.current_index = 0
+    else:
+        update_index(1)
+
+def previous_quote():
+    if st.session_state.current_index > 0:
+        update_index(-1)
+
+
+
+display_filename()
+
+
+
+with col1:
+    if st.button("⏮️ Previous", on_click=previous_quote):
+        pass
+
+with col3:
+    if st.button("Next ⏭️", on_click=next_quote):
+        pass
+
+imageName = image_names[st.session_state.current_index]
 
 st.write("The original image with the first model detecting the object and its bounding active.")
 st.image(box + imageName, width=720)
