bec149e639ee0d27e90ce7249aa460baeae4370c
Zulex <hizirwan@hotmail.com>
2024-07-14 20:42:35 +0000

    image comparison edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,7 +82,7 @@ st.write("I use a custom trained super resolution model to increase the image re
 image_comparison(
     img1= crop + imageName,
     img2= super + imageName,
-    use_column_width=True
+    make_responsive=True
 )
 
 st.write("Finally, using a custom trained keypoint detection model to detect individual points. These points are labeled. When the cables are detected correctly and the points of the circles align relatively well, we can assume the individual points are correctly labeled.")
@@ -90,6 +90,6 @@ st.write("Finally, using a custom trained keypoint detection model to detect ind
 image_comparison(
     img1= cropPose + imageName,
     img2= superPose + imageName,
-    use_column_width=True
+    make_responsive=True
 )
 
